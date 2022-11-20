4e35d0221c33a90da2a0b9105f8739f944720dca
HoosierPlayz <hoosierbenjamin@gmail.com>
2022-11-19 22:17:06 -0500

    first commit

---
+++ buyer_dashboard/buyer.js
@@ -0,0 +1,4 @@
+document.getElementById("profile_icon").addEventListener("mouseover", () => {
+  document.getElementById("profile_options_menu").style.display = "grid";
+  document.getElementById("profile_icon").style.opacity = "75%";
+});
